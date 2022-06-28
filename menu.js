48d1b7b7850ed666ef7c6c78662b2f7c1ff3b53e
UrielChame <102186138+UrielChame@users.noreply.github.com>
2022-06-28 17:00:06 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,12 @@
+let btnMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+btnMenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
+var nombre = document.getElementById('nombre');
+var nombre = document.getElementById('telefono');
+var nombre = document.getElementById('domicilio');
+var nombre = document.getElementById('promo');
+
